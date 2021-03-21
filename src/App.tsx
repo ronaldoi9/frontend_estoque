0823f78a33a9dbde0933e9bbb53275a64ab22f61
Ronaldo L. Inocencio Jr <ronaldolopesjunior@yahoo.com.br>
2021-03-21 18:47:31 -0300

    #refactor: refatorar arquivos

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,30 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { BrowserRouter } from 'react-router-dom'
+import { useDispatch } from 'react-redux'
 
+import './App.css'
 import Routes from './routes'
+import api from './services/api'
+import { Product } from './domain/product'
+import { loadProducts } from './store/product/actions'
+import { parseToProduct } from './utils/parse-to-product'
 
 function App() {
+
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    (async (): Promise<void> => {
+      const response = await api.get('/products')
+      const products = response.data.items.map((product: Product) => parseToProduct(product))
+      dispatch(loadProducts(products))
+    })()
+  }, [dispatch])
+
   return (
     <BrowserRouter>
       <Routes />
-    </BrowserRouter>
+    </BrowserRouter>    
   )
 }
 
